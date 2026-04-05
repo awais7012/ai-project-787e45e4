86265873a4e6c0448dd1f8b60569307fff97adc9
Romeo <ahmadawaisgithub@gmail.com>
2026-04-06 00:09:16 +0500

    Initial commit for src/hooks/useMousePosition.ts

---
+++ src/hooks/useMousePosition.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+interface MousePosition {
+  x: number;
+  y: number;
+}
+
+const useMousePosition = (): MousePosition => {
+  const [mousePosition, setMousePosition] = useState<MousePosition>({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updateMousePosition = (event: MouseEvent) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', updateMousePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, []);
+
+  return mousePosition;
+};
+
+export default useMousePosition;
